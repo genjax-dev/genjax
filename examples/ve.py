c9144b63a2a8bacac8f07af6cf4dbf2052ab6414
McCoy Becker <mccoybecker@gmail.com>
2025-04-17 00:04:24 -0400

    Init.

--- examples/ve.py
+++ examples/ve.py
@@ -7,8 +7,9 @@ from genjax import bernoulli, enum
 def prog():
     x = bernoulli.rv(probs=0.5)
     y = bernoulli.rv(probs=0.3)
-    p = cond(x, lambda: 0.3, lambda: 0.6)
-    return p > 0.5
+    p1 = cond(x, lambda: 0.3, lambda: 0.6)
+    p2 = cond(y, lambda: 0.1, lambda: 0.8)
+    return p1 + p2 > 0.5
 
 
 x, score = enum(prog)()
